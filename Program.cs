3abb61f9abf463c1de38f409233d46ac89b5da21
huaping-yu <34753593+huaping-yu@users.noreply.github.com>
2023-03-23 09:34:54 -0500

    fromBob2

--- Program.cs
+++ Program.cs
@@ -1,24 +1,10 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using Microsoft.Office.Interop.Outlook;
 namespace WatchEmail
 {
     class Program
     {
-        public static class Constants
-        {
-            public static readonly string[] keywordsBal = { " outstanding balance", " outstanding toll", " zc balance" };
-            public static readonly string[] keywordsListVeh = { " list of vehicles ", " list of transponders " };
-            public static readonly string[] keywordsChk = { " unable to locate check ", "not be located ", " cannot locate ", " ck #", " check #", " check#" };
-            public static readonly string[] keywordsDon = { " anil has code for this ", " need hv and vrb data ", " need data pulled soon after " };
-            public static readonly string[] keywordsSeema = { "img pull", "img request", "image pull", "image request", "pull images" };
-            public static readonly string[] keywordsVIN = { " Please provide VIN", " for the attached plates" };
-            public static readonly string[] keywordsBob = { "Email: rdigman@ntta.org" };
-            public static readonly string[] keywordsJinger = { "Email: Jelmore@ntta.org" };
-            public static readonly string keywordsScoff = "VTR_SCOFFLAW@txdmv.gov";
-        }
         static void Main(string[] args)
         {
             Program P = new();
@@ -45,25 +31,27 @@ namespace WatchEmail
 
             if (mail != null)
             {
-                bool findBal = Constants.keywordsBal.Any(s => mail.Body.ToLower().Contains(s));
-                bool listVeh = Constants.keywordsListVeh.Any(s => mail.Body.ToLower().Contains(s));
-                bool locateChk = Constants.keywordsChk.Any(s => mail.Body.ToLower().Contains(s));
-                bool forDon = Constants.keywordsDon.Any(s => mail.Body.ToLower().Contains(s));
-                bool forSeema = Constants.keywordsSeema.Any(s => mail.Body.ToLower().Contains(s));
-                bool provideVIN = Constants.keywordsVIN.Any(s => mail.Body.Contains(s));
-                bool fromScoff = mail.SenderEmailAddress == Constants.keywordsScoff;
-                bool fromBob = Constants.keywordsBob.Any(s => mail.Body.Contains(s));
-                bool fromJinger = Constants.keywordsJinger.Any(s => mail.Body.Contains(s));
+                bool findBal = ParseNGet.Program.Constants.keywordsBal.Any(s => mail.Body.ToLower().Contains(s));
+                bool listVeh = ParseNGet.Program.Constants.keywordsListVeh.Any(s => mail.Body.ToLower().Contains(s));
+                bool locateChk = ParseNGet.Program.Constants.keywordsChk.Any(s => mail.Body.ToLower().Contains(s));
+                bool forDon = ParseNGet.Program.Constants.keywordsDon.Any(s => mail.Body.ToLower().Contains(s));
+                bool forSeema = ParseNGet.Program.Constants.keywordsSeema.Any(s => mail.Body.ToLower().Contains(s));
+                bool provideVIN = ParseNGet.Program.Constants.keywordsVIN.Any(s => mail.Body.Contains(s));
+                bool fromScoff = ParseNGet.Program.Constants.keywordsScoff.Any(s=> mail.SenderEmailAddress.Contains(s));
+                bool fromBob = ParseNGet.Program.Constants.keywordsBob.Any(s => mail.Body.Contains(s));
+                bool fromBob2 = ParseNGet.Program.Constants.keywordsBob2.Any(s => mail.Body.Contains(s));
+                bool fromJinger = ParseNGet.Program.Constants.keywordsJinger.Any(s => mail.Body.Contains(s));
 
-                if (fromJinger || forSeema)
+                if (fromJinger || forSeema || fromBob2)
                 {
                     fileName = @"C:\Source\GetNZip\GetNZip\bin\Debug\z.exe";
                     if (forSeema) arg = "s";
+                    if (fromBob2) arg = "b";
                 }
 
                 if (provideVIN) arg = "v";
 
-                if (fromBob || findBal || listVeh || locateChk || forDon) arg = string.Empty;
+                if (fromBob || findBal || listVeh || locateChk || forDon || fromScoff) arg = string.Empty;
 
                 var proc = new Process
                 {
