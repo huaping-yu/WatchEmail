1149823c433642026d9e74f2b752a098ff6f0893
huaping-yu <34753593+huaping-yu@users.noreply.github.com>
2023-03-29 10:03:05 -0500

    noArg

--- Program.cs
+++ Program.cs
@@ -59,25 +59,27 @@ namespace WatchEmail
             if (mail != null)
             {
                 string mailBody = mail.Body;
+
+                bool forScoff = ParseNGet.Program.Constants.keywordsScoff.Any(s => mail.SenderEmailAddress.Contains(s));
+                bool forBob = ParseNGet.Program.Constants.keywordsBob.Any(s => mailBody.Contains(s));
+                bool forBob2 = ParseNGet.Program.Constants.keywordsBob2.Any(s => mailBody.Contains(s));
+                bool forJinger = ParseNGet.Program.Constants.keywordsJinger.Any(s => mailBody.Contains(s));
                 bool findBal = ParseNGet.Program.Constants.keywordsBal.Any(s => mailBody.ToLower().Contains(s));
                 bool listVeh = ParseNGet.Program.Constants.keywordsListVeh.Any(s => mailBody.ToLower().Contains(s));
                 bool locateChk = ParseNGet.Program.Constants.keywordsChk.Any(s => mailBody.ToLower().Contains(s));
                 bool forDon = ParseNGet.Program.Constants.keywordsDon.Any(s => mailBody.ToLower().Contains(s));
                 bool forSeema = ParseNGet.Program.Constants.keywordsSeema.Any(s => mailBody.ToLower().Contains(s));
                 bool provideVIN = ParseNGet.Program.Constants.keywordsVIN.Any(s => mailBody.Contains(s));
-                bool fromScoff = ParseNGet.Program.Constants.keywordsScoff.Any(s=> mail.SenderEmailAddress.Contains(s));
-                bool fromBob = ParseNGet.Program.Constants.keywordsBob.Any(s => mailBody.Contains(s));
-                bool fromBob2 = ParseNGet.Program.Constants.keywordsBob2.Any(s => mailBody.Contains(s));
-                bool fromJinger = ParseNGet.Program.Constants.keywordsJinger.Any(s => mailBody.Contains(s));
+                bool noArg = forBob || findBal || listVeh || locateChk || forDon || forScoff;
 
                 if (provideVIN) arg = "v";
-                else if (fromBob || findBal || listVeh || locateChk || forDon || fromScoff) arg = string.Empty;
-                else if (fromJinger || forSeema || fromBob2)
+                else if (noArg) arg = string.Empty;
+                else if (forJinger || forSeema || forBob2)
                 {
                     fileName = @"C:\Source\GetNZip\GetNZip\bin\Debug\z.exe";
                     arg = string.Empty;
                     if (forSeema) arg = "s";
-                    else if (fromBob2) arg = "b";
+                    else if (forBob2) arg = "b";
                 }
 
                 var proc = new Process
